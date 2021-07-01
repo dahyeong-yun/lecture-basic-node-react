9a71b3331ed6a736e7115abc9af07126a616883b
danny <gryb809@gmail.com>
2021-07-01 14:23:53 +0900

    :wrench: deploy 환경 고려하여 db 연결 정보 분리

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mongodb = require('./database')
+const config = require('./config/key')
 const express = require('express')
 const app = express()
 const port = 4000
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
 const mongoose = require('mongoose')
-mongoose.connect(mongodb.url, {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
