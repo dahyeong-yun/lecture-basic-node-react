c6c08da3a0a5618cbd59e7702c66393358469a08
danny <gryb809@gmail.com>
2021-07-01 15:46:58 +0900

    :sparkles: 로그인 기능 추가

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const config = require('./config/key')
 const express = require('express')
+const cookieParser = require('cookie-parser')
+
 const app = express()
 const port = 4000
 const { User } = require('./models/User')
 
-// application/x-www-form-urlencoded
-app.use(express.urlencoded({extended: true}))
-// application/json
-app.use(express.json());
+app.use(express.urlencoded({extended: true})) // application/x-www-form-urlencoded
+app.use(express.json()); // application/json
+app.use(cookieParser())
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -15,17 +16,51 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
+/**
+ * routers
+ */
 app.get('/', (req, res) => res.send('change test'))
 
 app.post('/register', (req, res) => {
-  // TODO 회원 가입 정보 추출
+  // 회원 가입 정보 추출
   const user = new User(req.body)
 
-  // TODO 디비에 저장
+  // 디비에 저장
   user.save((err, userInfo) => {
     if(err) return res.json({success: false, err})
     return res.status(200).json({
       success: true
+    })
+  })
+})
+
+app.post('/login', (req, res) => {
+  // 요청 계정 정보 찾기
+  User.findOne({email: req.body.email}, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      // 요청 계정 정보 존재시 비밀번호 인증
+      if(!isMatch) return res.json({
+        loginSuccess: false,
+        message: "비밀번호가 틀렸습니다."
+      })
+      
+      // 인증 완료시 토큰 생성
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err)
+        res.cookie("x-auth", user.token)
+        .status(200)
+        .json({
+          loginSuccess: true,
+          userId: user._id
+        })
+      })
     })
   })
 })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bctypt = require('bcrypt')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userScheme = mongoose.Schema({
     name: {
@@ -9,6 +12,10 @@ const userScheme = mongoose.Schema({
         type: String,
         trim: true,
         unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
     },
     lastname: {
         type: String,
@@ -26,6 +33,39 @@ const userScheme = mongoose.Schema({
         type: Number
     }
 })
+
+userScheme.pre('save', function( next ) {
+    const user = this;
+    if(user.isModified('password')) {
+        // 비밀 번호 암호화
+        bctypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+            bctypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+userScheme.methods.comparePassword = function(plainPassword, cb) {
+   bctypt.compare(plainPassword, this.password, function(err, isMatch) {
+       if(err) return cb(err)
+       cb(null, isMatch)
+   })
+}
+
+userScheme.methods.generateToken = function(cb) {
+    const user = this
+    user.token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userScheme)
 
