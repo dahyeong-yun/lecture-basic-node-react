2401cc4cc8abc39fafafdc1d378f292a71ecde18
danny <gryb809@gmail.com>
2021-07-02 13:19:16 +0900

    :wrench: 서버 동시 실행 설정 추가

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 
 const app = express()
-const port = 4000
+const port = 3001
 const { User } = require('./models/User')
 const { auth } = require('./middleware/auth')
 
